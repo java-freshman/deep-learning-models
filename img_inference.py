7ae251092af6b1a5adaf2519f35f03ed7d17d883
Admin <admin@AdmindeMacBook-Pro-91.local>
2018-08-15 14:29:11 +0800

    modi

--- img_inference.py
+++ img_inference.py
@@ -8,6 +8,8 @@ import time
 
 import gflags
 import numpy as np
+import pickle
+
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
 
@@ -16,6 +18,12 @@ from models.vgg16 import VGG16
 gflags.DEFINE_string('img_dir',
                      'images',
                      'path of the image folder')
+gflags.DEFINE_string('feat_dir',
+                     'extract_feature',
+                     'path of the feature folder')
+gflags.DEFINE_string('feat_ver',
+                     '20180815',
+                     'version of the extracted feature')
 FLAGS = gflags.FLAGS
 
 def load_model():
@@ -30,6 +38,7 @@ def main(argv):
 
     img_name_list = os.listdir(FLAGS.img_dir)
 
+    img_dict = dict()
     now = time.time()
     for img_name in img_name_list:
         img_path = os.path.join(FLAGS.img_dir, img_name)
@@ -38,10 +47,12 @@ def main(argv):
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
-
         pred = model.predict(x)
-        print('{} --> {}\n {} \n\n'.format(img_name, pred.shape, pred[0]))
 
+        img_dict[img_name] = pred[0]
+
+    feat_path = os.path.join(FLAGS.feat_dir, FLAGS.feat_ver)
+    pickle.dump(img_dict, open(feat_path, 'wb'))
     print("inference finished in {} seconds".format(time.time()-now))
 
 if __name__ == '__main__':
