4ccc55fa94d80e42274412fc9e9792befa24481e
Admin <admin@AdmindeMacBook-Pro-91.local>
2018-08-16 18:21:29 +0800

    modi

---
+++ img_feature_extraction.py
@@ -0,0 +1,93 @@
+"""
+    Author: wutenghu <wutenghu@chuangxin.com>
+    Date:   2018/8/15
+"""
+import os
+import sys
+import time
+
+import gflags
+import numpy as np
+import pickle
+
+import tensorflow as tf
+from keras.applications.imagenet_utils import preprocess_input
+from keras.preprocessing import image
+
+from models.vgg16 import VGG16
+
+gflags.DEFINE_string('img_dir',
+                     'images',
+                     'path of the image folder')
+gflags.DEFINE_string('feat_dir',
+                     'extract_feature',
+                     'path of the feature folder')
+gflags.DEFINE_string('embedding_vec',
+                     'embedding_vec',
+                     'embedding vectors')
+gflags.DEFINE_string('photo_pix',
+                     'photo_pix',
+                     'np type of the photos')
+gflags.DEFINE_string('extract_ver',
+                     '20180815',
+                     'version of the extracted feature')
+FLAGS = gflags.FLAGS
+
+def load_model():
+    model = VGG16(include_top=True, weights='imagenet')
+    return model
+
+def resize_image(array, image_size):
+    # print('resize to {}'.format(image_size))
+    resize_tensor = tf.image.resize_images(
+            array,
+            (image_size, image_size),
+            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    with tf.Session().as_default():
+        result = resize_tensor.eval()
+    return result
+
+def main(argv):
+    FLAGS(argv)
+
+    # load-in model
+    model = load_model()
+
+    img_name_list = os.listdir(FLAGS.img_dir)
+
+    embedding_vec_npy = list()
+    photo_pix_npy = list()
+    now = time.time()
+    count = 0
+    total_img_num = len(img_name_list)
+    for img_name in img_name_list:
+        if (count%50)==0:
+            print('process finished {:.2f} percentage.'.format(100*count/total_img_num))
+
+        img_path = os.path.join(FLAGS.img_dir, img_name)
+        img = image.load_img(img_path, target_size=(224, 224))
+
+        x = image.img_to_array(img)
+
+        photo_pix_npy.append(resize_image(x, 32))
+
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        pred = model.predict(x)
+
+        embedding_vec_npy.append(pred[0])
+        count += 1
+
+    embedding_vec_npy = np.asarray(embedding_vec_npy)
+    photo_pix_npy = np.asarray(photo_pix_npy, dtype='uint8')
+
+    embedding_vec_path = os.path.join(FLAGS.feat_dir, FLAGS.embedding_vec)
+    photo_pix_path = os.path.join(FLAGS.feat_dir, FLAGS.photo_pix)
+
+    np.save(embedding_vec_path+"/"+FLAGS.extract_ver, embedding_vec_npy)
+    np.save(photo_pix_path+"/"+FLAGS.extract_ver, photo_pix_npy)
+
+    print("process finished in {} seconds".format(time.time()-now))
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ img_tsne_visual.py
@@ -0,0 +1,48 @@
+"""
+    Author: wutenghu <wutenghu@chuangxin.com>
+    Date:   2018/8/16
+"""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import offsetbox
+
+from sklearn.manifold import TSNE
+
+# Scale and visualize the embedding vectors
+def plot_embedding(x_tsne, title=None):
+    x_min, x_max = np.min(x_tsne, 0), np.max(x_tsne, 0)
+    x_tsne = (x_tsne - x_min) / (x_max - x_min)
+
+    plt.figure()
+    ax = plt.subplot(111)
+
+    if hasattr(offsetbox, 'AnnotationBbox'):
+        shown_images = np.array([[1., 1.]])  # just something big
+        for i in range(x_tsne.shape[0]):
+            dist = np.sum((x_tsne[i] - shown_images) ** 2, 1)
+            if np.min(dist) <1e-3:
+                # don't show points that are too close
+                continue
+            shown_images = np.r_[shown_images, [x_tsne[i]]]
+            image_box = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(photos[i], cmap=plt.cm.gray_r), x_tsne[i])
+            ax.add_artist(image_box)
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
+
+
+print("start to load the data")
+x = np.load("extract_feature/embedding_vec/20180815.npy")
+photos = np.load("extract_feature/photo_pix/20180815.npy")
+
+x_tsne = TSNE(n_components=2).fit_transform(x)
+print(photos.shape)
+print(x_tsne.shape)
+
+assert photos.shape[0] == x_tsne.shape[0]
+
+plot_embedding(x_tsne)
+plt.show()
